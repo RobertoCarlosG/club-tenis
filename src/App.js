d0551c518c0ddc00a220c422f43559dd72d390cc
Roberto Gonzalez Avendaño <carlo-roberto50@hotmail.com>
2020-05-24 23:15:37 -0500

    Plantilla

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Redirect } from 'react-router-dom'
 import { Route } from 'react-router'
 import Login from './paginas/login'
+import Administrador from './paginas/administrador'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/administrador" component={Administrador} /> 
         <Redirect from="/" to ="/login" />
       </Switch>
     </BrowserRouter>
