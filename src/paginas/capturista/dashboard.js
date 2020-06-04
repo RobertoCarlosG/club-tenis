9122c1b04f3a3fe1b4c8bc74cba59fb0027f2015
Roberto Gonzalez Avendaño <carlo-roberto50@hotmail.com>
2020-06-04 03:13:06 -0500

    Revert "Solo EXCEL"
    This reverts commit 0782fce0b00914f701c1a6935ab7e95a029a9691.

--- src/paginas/capturista/dashboard.js
+++ src/paginas/capturista/dashboard.js
@@ -178,14 +178,14 @@ const Capturista = () => {
         
       </Grid>
 
-          <Dropzone className={classes.dropzone} ref={dropzoneRef} noClick noKeyboard accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" >
+          <Dropzone className={classes.dropzone} ref={dropzoneRef} noClick noKeyboard>
             {({getRootProps, getInputProps, acceptedFiles}) => {
               return (
               <div>
                 <Grid container direction="row" justify="space-between" >
                   <Grid item xs={6} sm={5}>
                     <div {...getRootProps({className: 'dropzone'})}>
-                      <input   {...getInputProps()} />
+                      <input  accept="application/vnd.openxmlformats-officedocument.wordprocessingml.document" {...getInputProps()} />
                       <p className={classes.con_dropzone}>Arrastra tu archivo aqui o seleccionalo</p>
                     </div>
                   </Grid>
